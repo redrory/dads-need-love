1a1f7dcdc141427f52996bc7fc806c3cd98ec807
Rory Walker <redrory@gmail.com>
2014-02-01 23:38:15 -0500

    input complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   mount_uploader :photo_two, AvatarUploader
   mount_uploader :photo_three, AvatarUploader
 
- validates_presence_of :name, :email, :photo_one, :photo_two, :photo_three, :memory_one, :memory_two, :memory_three
+ validates_presence_of :dad_name, :dad_email, :dad_bday, :photo_one, :photo_two, :photo_three, :memory_one, :memory_two, :memory_three
 
 end
